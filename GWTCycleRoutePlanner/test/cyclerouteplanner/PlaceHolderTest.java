8b32ace108708e9d019be2715cb86eb3c60a069f
Ben <benjgibbs@gmail.com>
2012-01-07 11:36:06 +0000

    basic working project

---
+++ GWTCycleRoutePlanner/test/cyclerouteplanner/PlaceHolderTest.java
@@ -0,0 +1,13 @@
+package cyclerouteplanner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlaceHolderTest {
+
+	@Test public void test() {
+		fail("Not yet implemented");
+	}
+
+}
