b08d3ba0defd739f0480caacbe458558dc833ea6
Ben <benjgibbs@gmail.com>
2012-01-07 20:22:27 +0000

    Basic drawing implemented Using slightly patched version of: http://code.google.com/p/gwt-google-maps-v3/

--- GWTCycleRoutePlanner/test/cyclerouteplanner/PlaceHolderTest.java
+++ GWTCycleRoutePlanner/test/cyclerouteplanner/PlaceHolderTest.java
@@ -1,13 +1,21 @@
 package cyclerouteplanner;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 import org.junit.Test;
 
+import com.google.gwt.maps.client.directions.DirectionsTravelMode;
+
 public class PlaceHolderTest {
 
-	@Test public void test() {
-		fail("Not yet implemented");
+	@Test public void whatDoesTravelModeReturn() {
+		DirectionsTravelMode mode = new DirectionsTravelMode();
+		String walking = mode.Walking();
+		String driving = mode.Driving();
+		assertThat(walking, equalTo("WALKING"));
+		assertThat(driving, equalTo("DRIVING"));
+		
 	}
-
 }
+
